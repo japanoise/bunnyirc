8e29bd2dc17291a22d7c4798d1cb30aa983b88c0
japanoise <japanoise@tfwno.gf>
2016-09-13 21:48:40 +0100

    formatting for mode changes

--- main.go
+++ main.go
@@ -15,6 +15,8 @@ func printmsg(msg *irc.Message){
 	switch msg.Command {
 	case "PRIVMSG":
 		fmt.Printf("%s/%s: %s\n",msg.Prefix.Name,msg.Params[0],msg.Params[1])
+	case "MODE":
+		fmt.Printf("%s sets mode %s on %s\n",msg.Prefix.Name,msg.Params[1],msg.Params[0])
 	case "NOTICE":
 		fmt.Printf("Notice from %s to %s: %s\n",msg.Prefix.Name,msg.Params[0],msg.Params[1])
 	case "001":
